4b293e84e9dc5e8c25dda91b4c4048417a4c7e9f
Victor Velasquez <victorcop90@gmail.com>
2021-05-10 19:59:18 -0500

    MasAcademyLab.Web

---
+++ MasAcademyLab.Web/Controllers/TalksController.cs
@@ -0,0 +1,89 @@
+﻿using MasAcademyLab.Service;
+using MasAcademyLab.Service.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasAcademyLab.Web.Controllers
+{
+    [Route("api/Trainings/{code}/[controller]")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ITalkService _talkService;
+        private readonly LinkGenerator _linkGenerator;
+        private readonly ITrainingService _trainingService;
+
+        public TalksController(ITalkService talkService, LinkGenerator linkGenerator, ITrainingService trainingService)
+        {
+            _talkService = talkService;
+            _linkGenerator = linkGenerator;
+            _trainingService = trainingService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string code, bool includeSpeakers = false)
+        {
+            var talks = await _talkService.GetTalksAsync(code, includeSpeakers);
+
+            if (talks == null || !talks.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(talks);
+        }
+
+        [HttpGet("{talkId:int}")]
+        public async Task<IActionResult> Get(string code, int talkId, bool includeSpeakers = false)
+        {
+            var talk = await _talkService.GetTalkAsync(code, talkId, includeSpeakers);
+
+            if (talk == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(talk);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string code, TalkCreationModel talkModel)
+        {
+            var talk = await _talkService.CreateTalkAsync(code, talkModel);
+
+            var url = _linkGenerator.GetPathByAction(HttpContext,
+                                                     "Get",
+                                                     values: new { code, talkId = talk.TalkId });
+
+            return Created(url, talk);
+        }
+
+        [HttpPut("{talkId:int}")]
+        public async Task<IActionResult> Put(string code, int talkId, TalkUpdateModel talkModel)
+        {
+            if (!await _trainingService.Exists(code) || !await _talkService.Exists(code, talkId))
+            {
+                return NotFound();
+            }
+
+            var talk = await _talkService.UpdateTalkAsync(code, talkId, talkModel);
+
+            return Ok(talk);
+        }
+
+        [HttpDelete("{talkId:int}")]
+        public async Task<IActionResult> Delete(string code, int talkId)
+        {
+            if ( !await _trainingService.Exists(code) || !await _talkService.Exists(code, talkId))
+            {
+                return NotFound();
+            }
+
+            await _talkService.DeleteTalkAsync(code, talkId);
+
+            return Ok();
+        }
+    }
+}
---
+++ MasAcademyLab.Web/Startup.cs
@@ -0,0 +1,69 @@
+using MasAcademyLab.Service.Extention;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+
+namespace MasAcademyLab.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceDependencies(Configuration);
+            services.AddControllers().AddNewtonsoftJson(setupAction =>
+            {
+                setupAction.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MasAcademyLab.Web", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MasAcademyLab.Web v1"));
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                    });
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
